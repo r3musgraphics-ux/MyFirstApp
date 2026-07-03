8d55f4e1249ef5f436f886ecfa76e37329b1527e
r3musgraphics-ux <r3musgraphics@gmail.com>
2026-07-04 00:13:31 +0800

    Initial Commit

--- MyFirstApp/src/app/index.tsx
+++ MyFirstApp/src/app/index.tsx
@@ -1,11 +1,13 @@
-import { Image, StyleSheet } from 'react-native';
+import { Image, StyleSheet, Text, TextInput } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-
-import { Text } from 'react-native';
 import { ThemedView } from '@/components/themed-view';
+import { useState } from 'react';
 
 
 export default function HomeScreen() {
+
+  const [userName, setUserName] = useState('');
+
   return (
     <ThemedView style={styles.container}>
       <SafeAreaView style={styles.screen}>
@@ -25,6 +27,15 @@ export default function HomeScreen() {
 
         <Text style={styles.school}>
           Mapúa Malayan Colleges Mindanao
+        </Text>
+
+        <TextInput style={styles.input}
+        placeholder="Test Input Here"
+        value={userName}
+        onChangeText={setUserName}
+        />
+        <Text style={styles.greeting}>
+         {userName}
         </Text>
 
         <Text style={styles.bio}>
@@ -65,12 +76,12 @@ const styles = StyleSheet.create({
 // i change the margin top of the course to -10 to make it look better and more aligned with the name text. 03/07/2026
   course: {
     fontSize: 15,
-    marginTop: -10,
+    marginTop: 5,
   },
 // i change the margin top of the school to -10 to make it look better and more aligned with the course text. 03/07/2026
   school: {
     fontSize: 14,
-    marginTop: -10,
+    marginTop: 5,
     textAlign: 'center',
   },
 // i change the font size of the bio text 03/07/2026
@@ -81,4 +92,20 @@ const styles = StyleSheet.create({
     fontStyle: 'italic',
   },
 
+// I added a TextInput so the user can type their name. 03/07/2026
+input: {
+  width: '80%',
+  borderWidth: 1,
+  borderColor: '#ccc',
+  borderRadius: 5,
+  padding: 10,
+  marginTop: 20,
+},
+
+// I added a greeting text that changes based on the user's input. 03/07/2026
+greeting: {
+  fontSize: 18,
+  marginTop: 15,
+  fontWeight: 'bold',
+},
 });
